58033fba686b1a9fa6d33709778d0cab70cb8884
Kevin <kev.saintigny.schmidt@gmail.com>
2020-08-09 12:11:55 +0200

    param middleware

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -4,6 +4,35 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+// ======== CHECK ID ===
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    // return to exit before next() if invalid
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+// ======== CHECK BODY ===
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
+// ======== GET ALL TOURS ===
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -17,18 +46,13 @@ exports.getAllTours = (req, res) => {
   });
 };
 
+// ======== GET TOUR ===
+
 exports.getTour = (req, res) => {
   console.log(req.params);
-
   const id = req.params.id * 1; // trick to convert into num
-  const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+  const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -37,6 +61,8 @@ exports.getTour = (req, res) => {
     },
   });
 };
+
+// ======== CREATE TOUR ===
 
 exports.createTour = (req, res) => {
   // console.log(req.body);
@@ -60,14 +86,9 @@ exports.createTour = (req, res) => {
   );
 };
 
-exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+// ======== UPDATE TOUR ===
 
+exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
@@ -76,14 +97,9 @@ exports.updateTour = (req, res) => {
   });
 };
 
-exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+// ======== DELETE TOUR ===
 
+exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
     data: null,
