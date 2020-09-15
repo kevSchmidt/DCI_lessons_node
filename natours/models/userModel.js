624da737f6164db88726c2ef6fe5b198ab2fbe74
Kevin <kev.saintigny.schmidt@gmail.com>
2020-09-15 08:07:10 +0200

    added user model

---
+++ natours/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// ======== User Schema ===
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please a name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
